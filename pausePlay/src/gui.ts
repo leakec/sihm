6e77a6aa80c5416a8581563ca5d1b8a5969f730a
leakec <leakec57@gmail.com>
2022-10-23 08:14:22 -0700

    Adding doc strings.

--- pausePlay/src/gui.ts
+++ pausePlay/src/gui.ts
@@ -2,88 +2,122 @@ import { GUI, GUIController } from "dat.gui";
 import * as THREE from "three";
 
 export class MyGui extends GUI {
-  video_controls: GUI;
-  pause_play_button: GUIController;
-  loop_button: GUIController;
-  time_slider: GUIController;
-  clip_action: THREE.AnimationAction;
-  looping: boolean;
-  max_time: number;
+    /** This is the folder of video controls. */
+    video_controls: GUI;
 
-  add_video_controls(
-    pause_play_func: () => void,
-    set_time_func: () => void,
-    clip_action: THREE.AnimationAction,
-  ) {
-    this.video_controls = this.addFolder("Video controls");
+    /** This is the GUI button to pause/play. */
+    pause_play_button: GUIController;
 
-    this.clip_action = clip_action;
+    /** This is the GUI button to loop once vs. loop infinitely. */
+    loop_button: GUIController;
 
-    // Add pause/play
-    this.pause_play_button = this.video_controls.add(
-      { "Pause/Play": pause_play_func },
-      "Pause/Play",
-    );
-    this.pause_play_button.name("\u23F8");
+    /** This is the GUI slider that can be used to change the video time. */
+    time_slider: GUIController;
 
-    // Add looping controls
-    var func = this.set_loop.bind(this); // Binding this to its method so we can pass it as a standalone function
-    this.loop_button = this.video_controls.add({ loop: func }, "loop");
-    this.loop_button.name("\uD83D\uDD03");
-    this.looping = true;
+    /** This is the clip action that is being controlled by the video controls. */
+    clip_action: THREE.AnimationAction;
 
-    // Add slider for time
-    this.max_time = this.clip_action.getClip().duration;
-    this.time_slider = this.video_controls.add(
-      { Time: 0.0 },
-      "Time",
-      0.0,
-      this.max_time,
-    );
-    this.time_slider.onChange(set_time_func);
-    this.video_controls.open();
-  }
+    /** Indicates wether the video is looping or not. */
+    looping: boolean;
 
-  pause() {
-    // Called when the animation is paused
-    this.pause_play_button.name("\u25B6");
-  }
+    /** Duration of the action clip. */
+    max_time: number;
 
-  play() {
-    // Called when the animation is played
-    this.pause_play_button.name("\u23F8");
-  }
+    /** This method adds the video controls.
+     * @param pause_play_func {() => void} A function used to pause/play the animation clip.
+     * @param set_time_func {() => void} A function used to set the time of the anmiation clip.
+     * @param clip_action {THREE.AnimationAction} The clip action that the video controls will control.
+     */
+    add_video_controls(
+        pause_play_func: () => void,
+        set_time_func: () => void,
+        clip_action: THREE.AnimationAction,
+    ) {
+        this.video_controls = this.addFolder("Video controls");
 
-  set_loop() {
-    if (this.looping) {
-      // Called when loop is disabled
-      this.looping = false;
-      this.clip_action.setLoop(THREE.LoopOnce, Infinity);
+        this.clip_action = clip_action;
 
-      // Change looping icon and mode
-      // Using surrogate pairs to render 5 digit unicode character.
-      // See: http://www.russellcottrell.com/greek/utilities/SurrogatePairCalculator.htm
-      this.loop_button.name("\uD83D\uDD02");
-    } else {
-      // Called when loop is enabled
-      this.looping = true;
-      this.clip_action.setLoop(THREE.LoopRepeat, Infinity);
-      this.clip_action.reset().play();
+        // Add pause/play
+        this.pause_play_button = this.video_controls.add(
+            { "Pause/Play": pause_play_func },
+            "Pause/Play",
+        );
+        this.pause_play_button.name("\u23F8");
 
-      // Change looping icon and mode
-      // Using surrogate pairs to render 5 digit unicode character.
-      // See: http://www.russellcottrell.com/greek/utilities/SurrogatePairCalculator.htm
-      this.loop_button.name("\uD83D\uDD03");
+        // Add looping controls
+        var func = this.set_loop.bind(this); // Binding this to its method so we can pass it as a standalone function
+        this.loop_button = this.video_controls.add({ loop: func }, "loop");
+        this.loop_button.name("\uD83D\uDD03");
+        this.looping = true;
+
+        // Add slider for time
+        this.max_time = this.clip_action.getClip().duration;
+        this.time_slider = this.video_controls.add(
+            { Time: 0.0 },
+            "Time",
+            0.0,
+            this.max_time,
+        );
+        this.time_slider.onChange(set_time_func);
+        this.video_controls.open();
     }
-  }
 
-  update_time() {
-    this.updateControllerWithoutCB(this.time_slider, this.clip_action.time);
-  }
+    /**
+     * This is called when the animation is paused.
+     */
+    pause() {
+        // Change the button symbol to use the "pause" unicode character
+        this.pause_play_button.name("\u25B6");
+    }
 
-  updateControllerWithoutCB(cont: GUIController, value: any) {
-    // Updates controller value without triggering the CB
-    cont.object[cont.property] = value;
-    cont.updateDisplay();
-  }
+    /**
+     * This is called when the animation is played.
+     */
+    play() {
+        // Change the button symbol to use the "play" unicode character
+        this.pause_play_button.name("\u23F8");
+    }
+
+    /**
+     * This function changes the loop status (single vs. infinite).
+     */
+    set_loop() {
+        if (this.looping) {
+            // Called when loop is disabled
+            this.looping = false;
+            this.clip_action.setLoop(THREE.LoopOnce, Infinity);
+
+            // Change looping icon and mode
+            // Using surrogate pairs to render 5 digit unicode character.
+            // See: http://www.russellcottrell.com/greek/utilities/SurrogatePairCalculator.htm
+            this.loop_button.name("\uD83D\uDD02");
+        } else {
+            // Called when loop is enabled
+            this.looping = true;
+            this.clip_action.setLoop(THREE.LoopRepeat, Infinity);
+            this.clip_action.reset().play();
+
+            // Change looping icon and mode
+            // Using surrogate pairs to render 5 digit unicode character.
+            // See: http://www.russellcottrell.com/greek/utilities/SurrogatePairCalculator.htm
+            this.loop_button.name("\uD83D\uDD03");
+        }
+    }
+
+    /**
+     * This method updates the time slider with the video animations current time.
+     */
+    update_time() {
+        this.updateControllerWithoutCB(this.time_slider, this.clip_action.time);
+    }
+
+    /**
+     * This method updates a controllers value without triggering its onChange function.
+     * @param cont {GUIController} The controller whose value is being changed.
+     * @param value {any} The value to change the controller to.
+     */
+    updateControllerWithoutCB(cont: GUIController, value: any) {
+        cont.object[cont.property] = value;
+        cont.updateDisplay();
+    }
 }
