8582f3f83c9616b135cfe7d53efa1b626931d47a
leakec <leakec57@gmail.com>
2023-01-21 20:28:35 -0800

    Now using GLSL shader as an example for #6

--- pausePlay/src/index.js
+++ pausePlay/src/index.js
@@ -24,10 +24,22 @@ camera_per.position.z = 25;
 // Create geometry
 const geometry = new THREE.SphereGeometry(1, 30, 30);
 const geometry2 = new THREE.BoxGeometry(1, 1, 1, 4, 4, 4);
-const material = new THREE.MeshBasicMaterial({
-    color: 0xffff00,
-    wireframe: true,
-    wireframeLinewidth: 2,
+const vertexShader = `
+varying vec3 v_normal;
+void main() {
+    v_normal = normal;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}
+`;
+const fragmentShader = `
+varying vec3 v_normal;
+void main() {
+  gl_FragColor = vec4(v_normal, 1.0);
+}
+`;
+const material = new THREE.ShaderMaterial({
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader,
 });
 const material2 = new THREE.MeshBasicMaterial({
     color: 0x00ff00,
